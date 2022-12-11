0424972a1d5462f9bcff2ee5910091056f61490c
Pao Chueh <pao.chueh@theomnieyes.com>
2022-12-11 15:32:22 +0800

    Add more test cases

--- contracts/interfaces/ITinyPoolTogether.sol
+++ contracts/interfaces/ITinyPoolTogether.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-interface ITinyPoolTogether{
+interface ITinyPoolTogether {
     // Define the event for when someone join the pool
-  event JoinPool(address user);
+    event JoinPool(address user);
 
-  // Define the event for when a round ends with a winner
-  event EndRound(address winner);
+    // Define the event for when a round ends with a winner
+    event EndRound(address winner);
 }
--- test/TinyPoolTogether.js
+++ test/TinyPoolTogether.js
@@ -15,8 +15,6 @@ describe("PoolTogether", () => {
   let user2;
 
   beforeEach(async function () {
-    this.timeout(60000);
-
     const tinyPoolTogetherFactory = await ethers.getContractFactory("TinyPoolTogether");
     contract = await tinyPoolTogetherFactory.deploy(
       INITIAL_POOL_SIZE,
@@ -45,5 +43,76 @@ describe("PoolTogether", () => {
 
     const winner = await contract.winner();
     expect(winner).to.be.oneOf([user1.address, user2.address]);
+  });
+
+  it("should reset the pool and user deposits when a round ends", async () => {
+    await contract.connect(user1).joinPool({ value: INITIAL_POOL_SIZE });
+
+    // increase 1 week for evm
+    await ethers.provider.send("evm_increaseTime", [ROUND_DURATION]);
+    await contract.endRound();
+
+    const pool = await contract.pool();
+    expect(pool).to.equal(0);
+    const deposit = await contract.deposits(user1.address);
+    expect(deposit).to.equal(0);
+  });
+
+  it("should allow users to purchase tickets and enter the lottery multiple times", async () => {
+    await contract.connect(user1).buyTicket({ value: TICKET_PRICE });
+    await contract.connect(user1).buyTicket({ value: TICKET_PRICE });
+    const tickets = await contract.tickets(user1.address);
+    expect(tickets).to.equal(2);
+  });
+
+  it("should reset the user's ticket count when a round ends", async () => {
+    await contract.connect(user1).joinPool({ value: INITIAL_POOL_SIZE });
+    await contract.connect(user1).buyTicket({ value: TICKET_PRICE });
+
+    // increase 1 week for evm
+    await ethers.provider.send("evm_increaseTime", [ROUND_DURATION]);
+    await contract.endRound();
+
+    const tickets = await contract.tickets(user1.address);
+    expect(tickets).to.equal(0);
+  });
+
+  it("should allow the contract owner to start a new round", async () => {
+    await contract.connect(user1).joinPool({ value: INITIAL_POOL_SIZE });
+
+    // increase 1 week for evm
+    await ethers.provider.send("evm_increaseTime", [ROUND_DURATION]);
+    await contract.endRound();
+
+    const roundActive = await contract.roundActive();
+    expect(roundActive).to.be.false;
+
+    await contract.startRound();
+    const newRoundActive = await contract.roundActive();
+    expect(newRoundActive).to.be.true;
+  });
+
+  it("should not allow users to join the pool if the round is not active", async () => {
+    await contract.connect(user1).joinPool({ value: INITIAL_POOL_SIZE });
+
+    // increase 1 week for evm
+    await ethers.provider.send("evm_increaseTime", [ROUND_DURATION]);
+    await contract.endRound();
+
+    await expect(contract.joinPool({ value: INITIAL_POOL_SIZE })).to.be.rejectedWith(
+      "Cannot join pool while round is not active"
+    );
+  });
+
+  it("should not allow users to buy tickets if the round is not active", async () => {
+    await contract.connect(user1).joinPool({ value: INITIAL_POOL_SIZE });
+
+    // increase 1 week for evm
+    await ethers.provider.send("evm_increaseTime", [ROUND_DURATION]);
+    await contract.endRound();
+
+    await expect(contract.buyTicket({ value: TICKET_PRICE })).to.be.rejectedWith(
+      "Cannot buy ticket while round is not active"
+    );
   });
 });
